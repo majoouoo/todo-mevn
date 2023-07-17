a425f06def71af57ac77a3fec93acfdc78534c26
Majoouoo <26277754+Majoouoo@users.noreply.github.com>
2023-07-17 21:24:10 +0200

    added a separate list for completed tasks

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -2,41 +2,58 @@
 import { ref, onMounted } from 'vue'
 import TaskBody from '../components/TaskBody.vue'
 
-let tasklist = ref()
+let tasklist = ref([])
+let tasklistCompleted = ref([])
 const fetchTasklist = () => {
   fetch('http://localhost:8080/api/tasklist')
     .then((response) => response.json())
     .then((body) => {
-      body.forEach((task) => (task.dateDue = new Date(Date.parse(task.dateDue)).toDateString()))
-      tasklist.value = body
+      for(let i = 0; i < body.length; i++) {
+        const task = body[i]
+        task.dateDue = new Date(Date.parse(task.dateDue)).toDateString()
+        task.index = i
+        task.complete ? tasklistCompleted.value.push(task) : tasklist.value.push(task)
+      }
     })
 }
 
 onMounted(() => fetchTasklist())
 
-const completeTask = (task) => {
-  task.complete = !task.complete
+const removeTaskFromList = (task) => {
   const taskId = task._id
+  if(task.complete) {
+    const index = tasklistCompleted.value.findIndex((taskFromList) => taskFromList._id == taskId)
+    tasklistCompleted.value.splice(index, 1)
+  } else {
+    const index = tasklist.value.findIndex((taskFromList) => taskFromList._id == taskId)
+    tasklist.value.splice(index, 1)
+  }
+}
 
-  fetch(`http://localhost:8080/api/completetask?id=${taskId}`, {
+const completeTask = (task) => {
+  fetch(`http://localhost:8080/api/completetask?id=${task._id}`, {
     method: 'PATCH'
   })
+    .then(() => {
+      removeTaskFromList(task)
+      task.complete = !task.complete
+      const newTasklist = task.complete ? tasklistCompleted : tasklist
+      newTasklist.value.push(task)
+      newTasklist.value.sort((a, b) => a.index - b.index)
+    })
 }
 
 const deleteTask = (task) => {
-  const taskId = task._id
-
-  fetch(`http://localhost:8080/api/deletetask?id=${taskId}`, {
+  fetch(`http://localhost:8080/api/deletetask?id=${task._id}`, {
     method: 'DELETE'
   }).then(() => {
-    const index = tasklist.value.findIndex((task) => task._id == taskId)
-    tasklist.value.splice(index, 1)
+    removeTaskFromList(task)
   })
 }
 </script>
 
 <template>
-  <div id="outerDiv">
+  <div id="tasklist">
     <TaskBody
       v-for="task of tasklist"
       :key="task._id"
@@ -45,10 +62,26 @@ const deleteTask = (task) => {
       @deleteTask="deleteTask(task)"
     ></TaskBody>
   </div>
+  <div id="tasklistCompleted">
+    <h3 id="tasklistCompletedTitle" v-if="tasklistCompleted.length > 0">Completed Tasks</h3>
+    <TaskBody
+      v-for="task of tasklistCompleted"
+      :key="task._id"
+      :task="task"
+      @completeTask="completeTask(task)"
+      @deleteTask="deleteTask(task)"
+    ></TaskBody>
+  </div>
+
 </template>
 
 <style scoped>
-#outerDiv {
+#tasklist {
   overflow-y: auto;
+}
+
+#tasklistCompletedTitle {
+  color: #595a75;
+  margin: 20px;
 }
 </style>
